f5a60cd5f9cdf0a65ec75fc866d694100e618df8
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-26 23:05:15 +0100

    created fetch hook

---
+++ src/components/AutocompleteSearch/hooks/useFetchOptions.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Option } from '../../../types/Global';
+
+const useFetchOptions = (query: string) => {
+  const [fetchedOptions, setFetchedOptions] = useState<Option[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchOptions = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          `https://spapi.dev/api/characters?search=${query}`,
+        );
+        const { data } = await response.json();
+        setFetchedOptions(
+          data.map((character: Character) => ({
+            id: character.id,
+            label: character.name,
+          })),
+        );
+      } catch (err) {
+        console.log(err);
+        setError('Error fetching data');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOptions();
+  }, [query]);
+
+  return { fetchedOptions, loading, error };
+};
+
+export default useFetchOptions;
