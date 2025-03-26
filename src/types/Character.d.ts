59d43e6e31c04af960487bf2f18f508cc5ebe8b1
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-26 17:24:21 +0100

    added Character type

---
+++ src/types/Character.d.ts
@@ -0,0 +1,17 @@
+interface Character {
+  id: number;
+  name: string;
+  age: number | null;
+  sex: string;
+  hair_color: string;
+  occupation: string;
+  grade: number | null;
+  religion: string;
+  voiced_by: string | null;
+  created_at: string;
+  updated_at: string;
+  url: string;
+  family: string;
+  relatives: Relative[];
+  episodes: string[];
+}
