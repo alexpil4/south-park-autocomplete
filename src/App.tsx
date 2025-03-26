92690166c2c2f3528e8918bc58adce00d3a931ee
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-26 17:24:57 +0100

    layout code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css';
+import AutoComplete from './components/AutocompleteSearch';
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className="welcome-title">
-          Welcome to{' '}
-          <span className="highlight">South Park Character Finder</span>
-        </h1>
-        <p className="description">
-          Search for your favorite South Park characters.
-        </p>
-      </div>
+      <h1 className="welcome-title">
+        Welcome to{' '}
+        <span className="highlight">South Park Character Finder</span>
+      </h1>
+      <p className="description">
+        Search for your favorite South Park characters.
+      </p>
+
+      <AutoComplete />
     </>
   );
 }
