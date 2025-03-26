c603d197938020691632ad6f1ba3607ccb51a930
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-26 23:04:55 +0100

    renamed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import AutoComplete from './components/AutocompleteSearch';
+import AutocompleteSearch from './components/AutocompleteSearch';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         Search for your favorite South Park characters.
       </p>
 
-      <AutoComplete />
+      <AutocompleteSearch />
     </>
   );
 }
